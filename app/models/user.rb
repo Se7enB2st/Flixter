b7453b1dbab70a5af4f51a60fee6ead0a5deb835
Se7enb2st <ethanhe1992@gmail.com>
2016-05-26 21:09:07 +0000

    Set up enrollment

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,13 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  :recoverable, :rememberable, :trackable, :validatable
+
+  def enrolled_in?(course)
+    enrolled_courses = enrollments.collect do |enrollment|
+      enrollment.course
+    end
+    return enrolled_courses.include?(course)
+  end
+
 end
